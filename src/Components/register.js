100a0a52a68299bf6f0222d8d72d43369ca89288
Drake <apple@apples-MacBook-Pro.local>
2021-04-05 15:34:23 -0400

    Added password inputs and the logic that they should match. Still need to add logic to send passwords to the backend

--- src/Components/register.js
+++ src/Components/register.js
@@ -1,19 +1,24 @@
 import react, { useState } from "react";
 import { Link } from "react-router-dom";
 function RegisterForm() {
-  function test(e) {
-    e.preventDefault();
-    fetch("http://localhost:8000/register", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        name: fullname,
-      }),
-    }).then(function (res) {
-      console.log(res);
-    });
+  function submission(e) {
+    if (fullname.pass1 !== fullname.pass2) {
+      e.preventDefault();
+      alert("Passwords do not match");
+    } else {
+      e.preventDefault();
+      fetch("http://localhost:8000/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: fullname,
+        }),
+      }).then(function (res) {
+        changeName({ first: "", last: "", email: "" });
+      });
+    }
   }
   function fChange(e) {
     const { name, value } = e.target;
@@ -23,9 +28,14 @@ function RegisterForm() {
         [name]: value,
       };
     });
-    console.log(fullname);
   }
-  const [fullname, changeName] = useState({ first: "", last: "", email: "" });
+  const [fullname, changeName] = useState({
+    first: "",
+    last: "",
+    email: "",
+    pass1: "",
+    pass2: "",
+  });
   return (
     <form>
       <input
@@ -49,8 +59,22 @@ function RegisterForm() {
         placeholder="Email"
         name="email"
       />
+      <input
+        onChange={fChange}
+        value={fullname.pass1}
+        type="password"
+        placeholder="Password"
+        name="pass1"
+      />
+      <input
+        onChange={fChange}
+        value={fullname.pass2}
+        type="password"
+        placeholder="Reenter Password"
+        name="pass2"
+      />
 
-      <button onClick={test} type="submit">
+      <button onClick={submission} type="submit">
         Submit
       </button>
     </form>
